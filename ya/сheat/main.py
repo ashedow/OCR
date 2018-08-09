6c572188213c73db18ffcfde09e09614bc8aa710
ya <ya.klementeva.compaero.ru>
2018-08-09 11:04:37 +0300

    no my/ only with import pytesseract

---
+++ ya/сheat/main.py
@@ -0,0 +1,33 @@
+from tesseract import image_to_string
+import io
+from PIL import Image
+import pytesseract
+from wand.image import Image as wi
+import pytesseract
+
+def imageReader():
+    im = Image.open("sample1.jpg")
+
+    text = pytesseract.image_to_string(im, lang = 'eng')
+
+    print(text)
+
+
+def pdfReader():
+    pdf = wi(filename = "sample2.pdf", resolution = 300)
+    pdfImage = pdf.convert('jpeg')
+
+    imageBlobs = []
+
+    for img in pdfImage.sequence:
+        imgPage = wi(image = img)
+        imageBlobs.append(imgPage.make_blob('jpeg'))
+
+    recognized_text = []
+
+    for imgBlob in imageBlobs:
+        im = Image.open(io.BytesIO(imgBlob))
+        text = pytesseract.image_to_string(im, lang = 'eng')
+        recognized_text.append(text)
+
+    print(recognized_text)
